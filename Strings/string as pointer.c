ebd2f111a9ead0d09a0b78521912f1a5f207ad98
Sheharyar Daym <147532749+SheharyarDaym@users.noreply.github.com>
2024-10-30 23:41:57 +0500

    Add files via upload

---
+++ Strings/string as pointer.c
@@ -0,0 +1,15 @@
+// String as pointer..
+
+#include<stdio.h>
+int main(){
+    char *name="Daym";
+    printf("%s\n", name);
+
+    name = "Sheharyar";
+    printf("%s\n", name);
+
+    name = "khan";
+    printf("%s", name);
+
+    return 0;
+}
